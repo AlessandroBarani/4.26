d52544841a1639ef41399c0178ea104a278c4851
nicob <nicob@MACBOOKPROALE>
2025-03-16 18:13:40 +0100

    Add project files.

---
+++ 4.26/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+const int LIMITE = 10;
+    int somma=0;
+for (int i= 1; i < LIMITE; i++)
+{
+    Console.Write("Inserisci il numero{0}: ", i);
+    int num = int.Parse(Console.ReadLine());
+    somma += num;
+    if (somma >= 100)
+
+        break;
+
+}
+
